7d69ababf2fd3e669ad79bc8ce4b28dde80193b2
mert-ozturk <mertozturk.seo.yonetim@gmail.com>
2023-01-23 15:30:29 +0300

    test3

--- src/App.js
+++ src/App.js
@@ -2,17 +2,37 @@ import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
 import SiriImage from './images/siri.png';
 import ProfileCard from './ProfileCard';
+import 'bulma/css/bulma.css';
 
 function App() {
   return (
     <div>
       <div>Personal Digital Assistans</div>
+    <div className="container">
+      <section className="section">
+        <div className="columns">
+          <div className="column is-4">
+          <ProfileCard 
+          title="Alexa" 
+          handle="@alexa99" 
+          image={AlexaImage} 
+          />
+          </div>
+          <div className="column is-4">
+          <ProfileCard 
+          title="Cortana" 
+          handle="@cortana5" 
+          image={SiriImage} 
+          />
+            </div>
+        </div>
+      </section>
+    </div>
+    
    
-    <ProfileCard title="Mert" handle="Ozturk" image={AlexaImage} />
-    <ProfileCard title="asd" handle="dfdf" image={SiriImage} />
     </div>
    
-  );
+  );  
 }
 
 export default App;
